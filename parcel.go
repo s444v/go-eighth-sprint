fcf034fa56820147a34b341e3495721299913c0a
s444v <dymnich02@gmail.com>
2025-04-20 13:31:48 +0300

    fix parcel.go

--- parcel.go
+++ parcel.go
@@ -91,6 +91,6 @@ func (s ParcelStore) SetAddress(number int, address string) error {
 func (s ParcelStore) Delete(number int) error {
 	// реализуйте удаление строки из таблицы parcel
 	// удалять строку можно только если значение статуса registered
-	_, err := s.db.Exec("DELETE FROM parcel WHERE number = :number AND status = registered", sql.Named("number", number))
+	_, err := s.db.Exec("DELETE FROM parcel WHERE number = :number AND status = :status", sql.Named("number", number), sql.Named("status", ParcelStatusRegistered))
 	return err
 }
